c980e2e1efeb9c40159c5a844cd20883dab7d2d4
Sugene Mulpuri <sugenemulpuri9@gmail.com>
2024-02-29 15:43:36 -0600

    phase 1 cc ramen rater v2 attempt 1

--- src/index.js
+++ src/index.js
@@ -1,21 +1,48 @@
-// index.js
+// // index.js
 
 // Callbacks
 const handleClick = (ramen) => {
   // Add code
+    const ramenName = document.getElementById('ramen-name');
+    const ramenRating = document.getElementById('ramen-rating');
+    ramenName.textContent = ramen.name;
+    ramenRating.textContent = ramen.rating;
+ 
 };
+
 
 const addSubmitListener = () => {
   // Add code
+  const ramenForm = document.getElementById("new-ramen");
+
+  ramenForm.addEventListener("submit", (e) => {
+      e.preventDefault();
+      addNewRamen();
+      ramenForm.reset();
+  })
 }
+
 
 const displayRamens = () => {
   // Add code
+      // fetch ramens from database
+      fetch("http://localhost:3000/ramens")
+      .then(res => res.json())
+      // once fetched, render each ramen with renderOneRamen()
+      .then(ramens => {
+        const image = document.createElement('img')
+          ramens.forEach(ramen => addNewRamen(ramen))
+          // also, show the details of the first ramen in the array
+          showRamenDetails(ramens[0]);
+      })
 };
 
-const main = () => {
+
+function main() {
   // Invoke displayRamens here
+  displayRamens();
   // Invoke addSubmitListener here
+  addSubmitListener();
 }
 
 main()
